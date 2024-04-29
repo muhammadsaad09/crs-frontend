fc202c11e2f403cde5b29d7f87d02c448903bc11
syedmohsindaniyalhaider <syedmohsindaniyalhaider@gmail.com>
2024-04-29 14:02:46 +0100

    bug: fixed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
       },
       boxShadow: {
         custom:
-          "rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px;",
+          "rgba(50, 50, 93, 0.25) 0px 30px 60px -12px, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px",
       },
     },
   },
